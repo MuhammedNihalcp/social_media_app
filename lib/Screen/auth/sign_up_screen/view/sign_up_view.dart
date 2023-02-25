a99a0abe343a4718da363531adbf978bf125a3c5
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-25 18:19:28 +0530

    sign_up screen

--- lib/Screen/auth/sign_up_screen/view/sign_up_view.dart
+++ lib/Screen/auth/sign_up_screen/view/sign_up_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ScreenSignUP extends StatelessWidget {
   const ScreenSignUP({super.key});
