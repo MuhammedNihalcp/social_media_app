e82dbbb9b0b81c3181abe81e9a3959e4daec4d70
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-25 18:19:06 +0530

    add sign_up screen

---
+++ lib/Screen/auth/sign_up_screen/view/sign_up_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ScreenSignUP extends StatelessWidget {
+  const ScreenSignUP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
