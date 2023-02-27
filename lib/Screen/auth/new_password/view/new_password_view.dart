3111ef29a6835715d28d8b75026d0dd7c17c49e9
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-27 11:37:15 +0530

    add new password screen file

---
+++ lib/Screen/auth/new_password/view/new_password_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/core/const_color.dart';
+
+class ScreenNewPassword extends StatelessWidget {
+  const ScreenNewPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: authbackgroundColor,
+      body: SafeArea(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
