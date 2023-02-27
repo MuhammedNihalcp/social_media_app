5f4a2c9d25a799c515ea41069b6c615c70790980
MuhammedNihalcp <nihalriju915@gmail.com>
2023-02-27 19:06:50 +0530

    update

--- lib/Screen/auth/new_password/view/new_password_view.dart
+++ lib/Screen/auth/new_password/view/new_password_view.dart
@@ -24,79 +24,82 @@ class ScreenNewPassword extends StatelessWidget {
     return Scaffold(
       backgroundColor: authbackgroundColor,
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Consumer<NewPasswordController>(
-          builder: (context, value, child) => Form(
-            key: formkey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: height * 0.1,
-                ),
-                const AuthHeadText(text: 'Reset Password'),
-                SizedBox(
-                  height: height * 0.1,
-                ),
-                const ForgotScreensHeadText(text: '  Enter New Password'),
-                kheight20,
-                const Text(
-                  'Your new password must be different from prviously used password',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: colorWhith,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 17,
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Consumer<NewPasswordController>(
+            builder: (context, value, child) => Form(
+              key: formkey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: height * 0.1,
                   ),
-                ),
-                kheight20,
-                TextFormFieldWidget(
-                  labelText: 'New Password',
-                  keyboardType: TextInputType.name,
-                  controller: value.newpasswordcontroller,
-                  obscureText: value.obscureText,
-                  onPressed: () {
-                    value.visibility();
-                  },
-                  icon: value.icon,
-                  validator: (newpassword) {
-                    return value.passwordValdation(newpassword);
-                  },
-                ),
-                kheight20,
-                TextFormFieldWidget(
-                  labelText: 'Conform Password',
-                  keyboardType: TextInputType.name,
-                  controller: value.conformpasswordcontroller,
-                  obscureText: value.obscureText,
-                  validator: (conformpassword) {
-                    return value.confirmpasswordValdation(conformpassword);
-                  },
-                ),
-                kheight20,
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(authbuttoncolor),
-                    fixedSize: MaterialStateProperty.all(
-                      Size(width * 0.8, height * 0.08),
+                  const AuthHeadText(text: 'Reset Password'),
+                  SizedBox(
+                    height: height * 0.1,
+                  ),
+                  const ForgotScreensHeadText(text: '  Enter New Password'),
+                  kheight20,
+                  const Text(
+                    'Your new password must be different from prviously used password',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: colorWhith,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17,
                     ),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                  ),
+                  kheight20,
+                  TextFormFieldWidget(
+                    labelText: 'New Password',
+                    keyboardType: TextInputType.name,
+                    controller: value.newpasswordcontroller,
+                    obscureText: value.obscureText,
+                    onPressed: () {
+                      value.visibility();
+                    },
+                    icon: value.icon,
+                    validator: (newpassword) {
+                      return value.passwordValdation(newpassword);
+                    },
+                  ),
+                  kheight20,
+                  TextFormFieldWidget(
+                    labelText: 'Conform Password',
+                    keyboardType: TextInputType.name,
+                    controller: value.conformpasswordcontroller,
+                    obscureText: value.obscureText,
+                    validator: (conformpassword) {
+                      return value.confirmpasswordValdation(conformpassword);
+                    },
+                  ),
+                  kheight20,
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(authbuttoncolor),
+                      fixedSize: MaterialStateProperty.all(
+                        Size(width * 0.8, height * 0.08),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
                     ),
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        log('add new password');
+                      }
+                    },
+                    child: const Text(
+                      'Continue',
+                      style: authButtonTextStyle,
+                    ),
                   ),
-                  onPressed: () {
-                    if (formkey.currentState!.validate()) {
-                      log('add new password');
-                    }
-                  },
-                  child: const Text(
-                    'Continue',
-                    style: authButtonTextStyle,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
