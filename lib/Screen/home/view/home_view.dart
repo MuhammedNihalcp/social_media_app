7cc7782dfe3c7619897e813bee77f88e5acab734
MuhammedNihalcp <nihalriju915@gmail.com>
2023-03-04 20:35:35 +0530

    add new home folder

---
+++ lib/Screen/home/view/home_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('home page'),
+        ),
+      ),
+    );
+  }
+}
