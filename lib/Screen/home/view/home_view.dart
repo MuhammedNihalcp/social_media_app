a00a49600a4900bbbf101071d0bd4ef19aa3f1f5
MuhammedNihalcp <nihalriju915@gmail.com>
2023-03-24 22:38:36 +0530

    add list view on home post

--- lib/Screen/home/view/home_view.dart
+++ lib/Screen/home/view/home_view.dart
@@ -46,114 +46,125 @@ class ScreenHome extends StatelessWidget {
       ),
       body: Consumer<SignUPController>(
         builder: (context, value, child) => SafeArea(
-          child: Column(
-            children: [
-              HomeStoryWidget(height: height),
-              const Divider(
-                thickness: 1,
-                color: colorWhith,
-              ),
-              Container(
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: DashedCircle(
-                        color: authbuttoncolor,
-                        dashes: 2,
-                        child: const CircleAvatar(),
-                      ),
-                      title: const Text(
-                        'Nihal',
-                        style: textwhitecolor,
-                      ),
-                      subtitle: const Text(
-                        'Muhammed nihal cp',
-                        style: textwhitecolor,
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.more_vert_rounded,
-                          color: colorWhith,
-                        ),
-                      ),
-                    ),
-                    const Image(
-                      image: NetworkImage(
-                          'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.favorite_border,
-                            color: colorWhith,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.comment_rounded,
-                            color: colorWhith,
-                          ),
-                        ),
-                        Transform.rotate(
-                          angle: -30 * pi / 180,
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.send,
-                              color: colorWhith,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                HomeStoryWidget(height: height),
+                const Divider(
+                  thickness: 1,
+                  color: colorWhith,
+                ),
+                ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: DashedCircle(
+                              color: authbuttoncolor,
+                              dashes: 2,
+                              child: const CircleAvatar(),
+                            ),
+                            title: const Text(
+                              'Nihal',
+                              style: textwhitecolor,
+                            ),
+                            subtitle: const Text(
+                              'Muhammed nihal cp',
+                              style: textwhitecolor,
+                            ),
+                            trailing: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.more_vert_rounded,
+                                color: colorWhith,
+                              ),
                             ),
                           ),
-                        ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.bookmark_border,
-                            color: colorWhith,
+                          const Image(
+                            image: NetworkImage(
+                                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
                           ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: const [
-                        kwidth10,
-                        Text(
-                          '322 likes',
-                          style: textwhitecolor,
-                        ),
-                      ],
-                    ),
-                    kheight10,
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: ReadMoreText(
-                        'qwertyuiop[]asdfghjklzxcvbnm,qwertyuioasdfghjklzxcvbnmqwertyuiopasdfghjklcvbnmqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
-                        style: textwhitecolor,
-                        trimLines: 2,
-                        colorClickableText: Colors.pink,
-                        trimMode: TrimMode.Line,
-                        trimCollapsedText: 'more',
-                        trimExpandedText: 'less',
-                        moreStyle: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: colorWhith,
-                        ),
-                        lessStyle: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: colorWhith,
-                        ),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.favorite_border,
+                                  color: colorWhith,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.comment_rounded,
+                                  color: colorWhith,
+                                ),
+                              ),
+                              Transform.rotate(
+                                angle: -30 * pi / 180,
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.send,
+                                    color: colorWhith,
+                                  ),
+                                ),
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.bookmark_border,
+                                  color: colorWhith,
+                                ),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: const [
+                              kwidth10,
+                              Text(
+                                '322 likes',
+                                style: textwhitecolor,
+                              ),
+                            ],
+                          ),
+                          kheight10,
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: ReadMoreText(
+                              'qwertyuiop[]asdfghjklzxcvbnm,qwertyuioasdfghjklzxcvbnmqwertyuiopasdfghjklcvbnmqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
+                              style: textwhitecolor,
+                              trimLines: 2,
+                              colorClickableText: Colors.pink,
+                              trimMode: TrimMode.Line,
+                              trimCollapsedText: 'more',
+                              trimExpandedText: 'less',
+                              moreStyle: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: colorWhith,
+                              ),
+                              lessStyle: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: colorWhith,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+                    );
+                  },
+                  separatorBuilder: (context, index) => kheight10,
+                  itemCount: 10,
                 ),
-              )
-            ],
+                kheight20
+              ],
+            ),
           ),
         ),
       ),
