47c76a27b328910ca4de6166f3c8b5cd36b2c6a0
MuhammedNihalcp <nihalriju915@gmail.com>
2023-03-29 00:04:49 +0530

    add staggered grid view on search view file

--- lib/Screen/search_screen/view/search_view.dart
+++ lib/Screen/search_screen/view/search_view.dart
@@ -18,23 +18,52 @@ class ScreenSearch extends StatelessWidget {
       body: SafeArea(
         child: Consumer<SearchController>(
           builder: (context, value, child) {
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: CupertinoSearchTextField(
-                    prefixInsets:
-                        const EdgeInsetsDirectional.fromSTEB(10, 4, 5, 3),
-                    backgroundColor: const Color.fromARGB(82, 158, 158, 158),
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      color: colorWhith,
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: CupertinoSearchTextField(
+                      prefixInsets:
+                          const EdgeInsetsDirectional.fromSTEB(10, 4, 5, 3),
+                      backgroundColor: const Color.fromARGB(82, 158, 158, 158),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        color: colorWhith,
+                      ),
+                      placeholderStyle: textwhitecolor,
+                      controller: value.searchcontroller,
                     ),
-                    placeholderStyle: textwhitecolor,
-                    controller: value.searchcontroller,
                   ),
-                ),
-              ],
+                  GridView.custom(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverQuiltedGridDelegate(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                      pattern: [
+                        const QuiltedGridTile(4, 2),
+                        const QuiltedGridTile(1, 2),
+                        const QuiltedGridTile(1, 2),
+                        const QuiltedGridTile(1, 2),
+                        const QuiltedGridTile(1, 2)
+                      ],
+                    ),
+                    childrenDelegate: SliverChildBuilderDelegate(
+                      childCount: 20,
+                      (context, index) => Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://picsum.photos/id/18/500/500?random=$index'),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           },
         ),
